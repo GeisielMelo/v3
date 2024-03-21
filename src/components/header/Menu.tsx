6165d9446d6806da22544c2a0aecab79969cd9f7
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-21 15:28:16 -0300

    feat: implemented a mobile menu for a better compatibility with small devices

---
+++ src/components/header/Menu.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button'
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { HamburgerMenuIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import { css } from './index'
+import { useNavigate } from 'react-router-dom'
+import { useDarkMode } from '@/hooks/useDarkMode'
+
+export const Menu: React.FC = () => {
+  const { isDarkMode, setDarkMode } = useDarkMode()
+  const navigate = useNavigate()
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant='outline' className='md:hidden'>
+          <HamburgerMenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className='flex flex-col w-full items-center py-10 bg-slate-50 dark:bg-slate-950/80'>
+        <SheetClose className={css.option} onClick={() => navigate('/')}>
+          Home
+        </SheetClose>
+        <SheetClose className={css.option} onClick={() => navigate('/about')}>
+          About
+        </SheetClose>
+        <SheetClose className={css.option} onClick={() => navigate('/projects')}>
+          Projects
+        </SheetClose>
+        <SheetClose className={css.option} onClick={() => navigate('/contact')}>
+          Contact
+        </SheetClose>
+        <SheetClose className={css.option} onClick={() => navigate('/resume')}>
+          Resume
+        </SheetClose>
+        <Button variant='ghost' className='mt-auto' onClick={() => setDarkMode(!isDarkMode)}>
+          {isDarkMode ? <SunIcon /> : <MoonIcon />}
+        </Button>
+      </SheetContent>
+    </Sheet>
+  )
+}
