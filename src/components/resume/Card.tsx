f71bf09d9ad7cecf840771c0979c021cc9c9308a
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-22 14:39:13 -0300

    feat: implemented resume section

---
+++ src/components/resume/Card.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+type CardProps = {
+  data: {
+    title: string
+    period: string
+    locale: string
+    description: string
+    technologies: string[]
+  }
+}
+
+export const Card: React.FC<CardProps> = ({ data }) => {
+  const [openCard, setOpenCad] = useState(false)
+
+  return (
+    <div
+      className='font-Lexend block md:flex gap-0 md:gap-8 w-full px-0 md:px-6 py-6 my-6 rounded-md shadow-md dark:shadow-light-shadow transition-all cursor-pointer'
+      onClick={() => setOpenCad(!openCard)}
+    >
+      <h1 className='whitespace-nowrap text-slate-500 font-Inter text-sm'>{data.period}</h1>
+
+      <div className='w-full'>
+        <h1>{data.title}</h1>
+        <h2 className='text-slate-500 text-sm font-semibold'>{data.locale}</h2>
+        {openCard && <p className='my-4 text-slate-500'>{data.description}</p>}
+
+        <div className='flex flex-wrap gap-2 mt-6'>
+          {data.technologies.map((element, key) => (
+            <p
+              className='opacity-70 text-xs px-2 py-1 rounded-full cursor-default dark:hover:text-custom-green hover:opacity-100 border dark:border-custom-green dark:bg-custom-navy-light'
+              key={key}
+            >
+              {element}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,7 +1,41 @@
+import { Card } from '@/components/resume/Card'
+import { Button } from '@/components/ui/button'
+import { DownloadIcon } from '@radix-ui/react-icons'
+
+const data = [
+  {
+    title: 'Frontend Developer',
+    period: 'Out 2020 — Fev 2022',
+    locale: 'Brazil',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    technologies: ['React', 'TypeScript', 'TailwindCSS'],
+  },
+  {
+    title: 'Backend Developer',
+    period: 'Out 2022 — Fev 2024',
+    locale: 'USA',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+    technologies: ['Angular', 'TypeScript', 'TailwindCSS'],
+  },
+]
+
 const Resume: React.FC = () => {
   return (
-    <section className='max-w-5xl w-full'>
-      <h1>Resume</h1>
+    <section className='py-28 max-w-5xl w-full'>
+      <h1 className='font-Inter text-[clamp(20px,4vw,25px)] leading-none font-semibold tracking-tighter text-accent-foreground'>
+        Where I’ve Worked
+      </h1>
+      {data.map((element, key) => (
+        <Card data={element} key={key} />
+      ))}
+      <div className='flex justify-center'>
+        <Button
+          className='cursor-pointer transition-all dark:hover:text-custom-green dark:hover:bg-slate-800/30'
+          variant='ghost'
+        >
+          <DownloadIcon />
+        </Button>
+      </div>
     </section>
   )
 }
