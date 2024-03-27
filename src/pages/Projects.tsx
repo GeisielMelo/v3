faadba7560519ee364604191673edb62e3af7bc0
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-26 21:33:31 -0300

    feat: implemented projects section

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,14 @@
+import { Featured } from '@/components/Featured'
+import { GitProjects } from '@/components/GitProjects'
+
 const Projects: React.FC = () => {
   return (
     <section className='py-28 max-w-5xl w-full'>
-      <h1 className='font-Inter text-[clamp(20px,4vw,25px)] leading-none font-semibold tracking-tighter text-accent-foreground'>
-        Projects
+      <h1 className='font-Inter text-[clamp(20px,4vw,25px)] leading-none font-semibold tracking-tighter text-accent-foreground mb-8'>
+        Some Things I’ve Built
       </h1>
+      <Featured />
+      <GitProjects />
     </section>
   )
 }
