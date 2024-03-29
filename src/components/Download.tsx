0601102f7d06aa1eb813764b473c6113cf0e66f6
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-28 23:05:12 -0300

    feat: removed invasive function to download a pdf file; added a preview button

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -1,31 +1,12 @@
 import { DownloadIcon } from '@radix-ui/react-icons'
 import { Button } from './ui/button'
 
-const Download:React.FC = () => {
-  const handleDownload = async () => {
-    try {
-      const response = await fetch('static/resume.pdf')
-
-      if (!response.ok) throw new Error('Something went wrong.')
-
-      const blob = await response.blob()
-      const blobUrl = window.URL.createObjectURL(new Blob([blob]))
-      const link = document.createElement('a')
-      link.href = blobUrl
-      link.setAttribute('download', 'resume.pdf')
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-    } catch (error) {
-      console.error('Something went wrong.')
-    }
-  }
-
+const Download: React.FC = () => {
   return (
     <Button
       className='cursor-pointer transition-all dark:hover:text-custom-green dark:hover:bg-slate-800/30'
       variant='ghost'
-      onClick={handleDownload}
+      onClick={() => window.open('static/resume.pdf', '_blank')}
     >
       <DownloadIcon />
     </Button>
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -5,15 +5,9 @@ import data from '../assets/json/worked.json'
 const Resume: React.FC = () => {
   return (
     <section className='py-10 max-w-5xl w-full'>
-      <h1 className='font-Inter text-[clamp(20px,4vw,25px)] leading-none font-semibold tracking-tighter text-accent-foreground'>
-        Where I’ve Worked
-      </h1>
-      {data.map((element, key) => (
-        <Worked data={element} key={key} />
-      ))}
-      {/* <div className='flex justify-center'>
-        <Download />
-      </div> */}
+      <h1 className='font-Inter text-[clamp(20px,4vw,25px)] leading-none font-semibold tracking-tighter text-accent-foreground'>Where I’ve Worked</h1>
+      {data.map((element, key) => ( <Worked data={element} key={key} /> ))}
+      {/* <div className='flex justify-center'><Download /></div>  */}
     </section>
   )
 }
