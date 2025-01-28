f5dfc5a5acd4c915e2bf6f6d1f072793fcb765a6
Geisiel <geisiel.nascimento@gmail.com>
2025-01-28 11:31:21 -0300

    feat: updated project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,59 +2,56 @@ import '@/styles/global.css'
 
 import { getLocale, getMessages } from 'next-intl/server'
 import { NextIntlClientProvider } from 'next-intl'
-import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://www.geisiel.com/'),
-  title: {
-    default: 'Geisiel | Web Developer',
-    template: '%s | Geisiel',
-  },
-  authors: {
-    name: 'Geisiel',
-    url: 'https://www.linkedin.com/in/geisiel',
-  },
-  description:
-    'Web developer with years of experience specializing in creating optimized, responsive sites for exceptional user experiences.',
-  applicationName: 'GeisielMelo',
-  publisher: 'Geisiel',
-  archives: '/static/resume.pdf',
-  creator: 'Geisiel Nascimento',
-  icons: {
-    icon: '/favicon.ico',
-  },
-  keywords: [
-    'web developer',
-    'web developer geisiel',
-    'web developer geisiel melo',
-    'web developer geisiel nascimento',
-    'geisiel open to work, open to work, geisiel melo',
-  ],
-  classification: 'Portfolio - GeisielMelo',
-  alternates: {
-    canonical: '/',
-  },
-  openGraph: {
-    images: ['/img/social.png'],
-    type: 'website',
-    url: '/',
-    locale: 'en_US',
-    description: "I'm the right developer to optimize your Google positioning with quality and results.",
-  },
-  twitter: {
-    images: ['/img/social.png'],
-    description: "I'm the right developer to optimize your Google positioning with quality and results.",
-  },
-  verification: {
-    google: process.env.APP_GOOGLE_SITE_VERIFICATION,
-    other: {
-      'msvalidate.01': process.env.APP_BING_SITE_VERIFICATION as string,
+export async function generateMetadata() {
+  const locale = await getLocale()
+  return {
+    metadataBase: new URL('https://www.geisiel.com/'),
+    title: {
+      default: 'Geisiel | Web Developer',
+      template: '%s | Geisiel',
     },
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
+    authors: {
+      name: 'Geisiel',
+      url: 'https://www.linkedin.com/in/geisiel',
+    },
+    description:
+      'Web developer with years of experience specializing in creating optimized, responsive sites for exceptional user experiences.',
+    applicationName: 'GeisielMelo',
+    publisher: 'Geisiel',
+    archives: '/static/resume.pdf',
+    creator: 'Geisiel Nascimento',
+    icons: { icon: '/favicon.ico' },
+    keywords: [
+      'web developer',
+      'web developer geisiel',
+      'web developer geisiel melo',
+      'web developer geisiel nascimento',
+      'geisiel open to work, open to work, geisiel melo',
+    ],
+    classification: 'Portfolio - GeisielMelo',
+    alternates: { canonical: `/${locale}` },
+    openGraph: {
+      images: ['/img/social.png'],
+      type: 'website',
+      url: '/',
+      description: "I'm the right developer to optimize your Google positioning with quality and results.",
+    },
+    twitter: {
+      images: ['/img/social.png'],
+      description: "I'm the right developer to optimize your Google positioning with quality and results.",
+    },
+    verification: {
+      google: process.env.APP_GOOGLE_SITE_VERIFICATION,
+      other: {
+        'msvalidate.01': process.env.APP_BING_SITE_VERIFICATION as string,
+      },
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+  }
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -17,14 +17,17 @@ export const Header: FC<{ locale: string }> = ({ locale }) => {
   return (
     <header className='lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24'>
       <div>
-        <h1 className='text-4xl font-bold tracking-tight text-[#ededed] sm:text-5xl'>{t('name')}</h1>
-        <h2 className='mt-3 text-lg font-medium tracking-tight text-[#ededed] sm:text-xl'>{t('dev')}</h2>
+        <span className='text-4xl font-bold tracking-tight text-[#ededed] sm:text-5xl'>{t('name')}</span>
+        <h1 className='mt-3 text-lg font-medium tracking-tight text-[#ededed] sm:text-xl'>{t('dev')}</h1>
         <p className='mt-4 max-w-xs leading-normal'>{t('description')}</p>
 
         <nav className='nav hidden lg:block'>
           <ul className='flex flex-col gap-1 mt-16 w-max'>
             {keys.map((key, index) => (
-              <li key={index} className='flex text-xs font-bold uppercase tracking-widest hover:pl-4 hover:text-white transition-all cursor-pointer'>
+              <li
+                key={index}
+                className='flex text-xs font-bold uppercase tracking-widest hover:pl-4 hover:text-white transition-all cursor-pointer'
+              >
                 <Link href={locale + '/' + t(`options.${key}.href`)}>{t(`options.${key}.title`)}</Link>
               </li>
             ))}
@@ -32,7 +35,7 @@ export const Header: FC<{ locale: string }> = ({ locale }) => {
         </nav>
       </div>
 
-      <div className='mt-8 flex items-center gap-2' aria-label='Social media'>
+      <div className='mt-8 flex items-center gap-2'>
         <a
           href='https://github.com/GeisielMelo'
           className='shrink-0 text-xs bg-slate-800/50 hover:text-white transition-all p-1 rounded-sm'
